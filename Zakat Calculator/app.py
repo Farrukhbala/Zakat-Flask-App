90244a1896413cb96ef4e77be6219c217057a49e
Farrukh khan <35742633+Farrukhbala@users.noreply.github.com>
2019-04-24 11:02:57 -0700

    Zakat App with some functionality

--- Zakat Calculator/app.py
+++ Zakat Calculator/app.py
@@ -34,38 +34,54 @@ def is_logged_in(f):
   return wrap
 
 
-
+# ================================INDEX PAGE===================================
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+
+# ================================INFORMATION PAGE===================================
+
 @app.route('/information')
 def information():
   return render_template('information.html')
 
-
+# ================================CALCULATE ZAKAT FORM CLASS===================================
 class CalculateZakat(Form):
-  gold = StringField('Gold' ,[validators.length(min=2 , max=30)])
-  silver = StringField('Silver' ,[ validators.length(min=2 , max=30)])
-  cash = StringField('Cash' , [validators.length(min=5 , max= 30)])
-  properties = StringField('Property' ,[ validators.length(min=2 , max=30)])
-  liabilities = StringField('Liabilities' , [validators.length(min=5 , max= 30)])
+  gold = StringField('Gold in (Cash)' ,[validators.length(min=2 , max=30)])
+  silver = StringField('Silver in (Cash)' ,[ validators.length(min=2 , max=30)])
+  cash = StringField('Cash in Hand' , [validators.length(min=5 , max= 30)])
+  properties = StringField('Property in (Cash)' ,[ validators.length(min=2 , max=30)])
+  liabilities = StringField('Liabilities in (Cash)' , [validators.length(min=5 , max= 30)])
 
-
+# ================================CALCULATE ZAKAT PAGE===================================
 
 @app.route('/calculate' ,methods = ['GET' , 'POST'])
 def calculate():
   form = CalculateZakat(request.form)
 
-  gold = form.gold.data
-  silver = form.silver.data
-  cash = form.cash.data
-  properties =  form.properties.data
-  liabilities =  form.liabilities.data
+  if request.method == "POST" and form.validate():
+
+    gold = int(form.gold.data)
+    silver = int(form.silver.data)
+    cash = int(form.cash.data)
+    properties =  int(form.properties.data)
+    liabilities =  int(form.liabilities.data)
+
+    if (gold < 10000) and (silver < 10000) and (cash < 10000) and (properties < 10000) and (liabilities < 10000):
+      
+      return render_template('not-eligible.html') 
+    else:
+      zakat = (gold + silver + cash + properties + liabilities)//2.5
+      return render_template('zakat.html' , zakat = zakat) 
+
+
+
   
   return render_template('calculate.html' , form = form)
 
+# ================================CALCULATE ZAKAT PAGE===================================
 #REGISTRATION FORMS START
 
 class RegistrationForm(Form):
@@ -78,6 +94,7 @@ class RegistrationForm(Form):
   confirm = PasswordField('Comfrim Password' , [validators.DataRequired()])
 
 
+# ================================REGISTRATION PAGE===================================
 
 @app.route('/registration' , methods = ['GET' , 'POST'])
 def registration():
@@ -104,6 +121,7 @@ def registration():
 
 #REGISTRATION FORMS END
 
+# ================================LOGIN FORMS PAGE===================================
 
 #LOGIN FORMS START
 
@@ -145,7 +163,9 @@ def login():
   return render_template('login.html' , form = form)
 
 
+#LOGIN FORMS ENDS
+
+
 if __name__ == "__main__":
   app.secret_key = 'secret123'
   app.run(debug=True , port = 3000)
-    
